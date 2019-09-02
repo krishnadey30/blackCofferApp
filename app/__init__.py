e01b23b001c348b2cd076ef31f464f757b90bc40
krishnadey30 <krishnakumardey.dey@gmail.com>
2019-09-02 21:20:53 +0530

    Adds PUT,DELETE,GET using a id

--- app/__init__.py
+++ app/__init__.py
@@ -51,4 +51,42 @@ def create_app(config_name):
             response = jsonify(results)
             response.status_code = 200
             return response
+    
+    @app.route('/bucketlists/<int:id>', methods=['GET', 'PUT', 'DELETE'])
+    def bucketlist_manipulation(id, **kwargs):
+     # retrieve a buckelist using it's ID
+        bucketlist = Bucketlist.query.filter_by(id=id).first()
+        if not bucketlist:
+            # Raise an HTTPException with a 404 not found status code
+            abort(404)
+
+        if request.method == 'DELETE':
+            bucketlist.delete()
+            return {
+            "message": "bucketlist {} deleted successfully".format(bucketlist.id) 
+         }, 200
+
+        elif request.method == 'PUT':
+            name = str(request.data.get('name', ''))
+            bucketlist.name = name
+            bucketlist.save()
+            response = jsonify({
+                'id': bucketlist.id,
+                'name': bucketlist.name,
+                'date_created': bucketlist.date_created,
+                'date_modified': bucketlist.date_modified
+            })
+            response.status_code = 200
+            return response
+        else:
+            # GET
+            response = jsonify({
+                'id': bucketlist.id,
+                'name': bucketlist.name,
+                'date_created': bucketlist.date_created,
+                'date_modified': bucketlist.date_modified
+            })
+            response.status_code = 200
+            return response
+
     return app

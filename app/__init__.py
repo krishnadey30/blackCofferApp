eca29edfc24d0aebe3d091933a0433da57351109
krishnadey30 <krishnakumardey.dey@gmail.com>
2019-09-02 21:18:29 +0530

    Adds GET/POST

--- app/__init__.py
+++ app/__init__.py
@@ -2,6 +2,7 @@
 
 from flask_api import FlaskAPI
 from flask_sqlalchemy import SQLAlchemy
+from flask import request, jsonify, abort
 
 # local import
 from instance.config import app_config
@@ -11,10 +12,43 @@ db = SQLAlchemy()
 
 
 def create_app(config_name):
+    from app.models import Bucketlist
+
     app = FlaskAPI(__name__, instance_relative_config=True)
     app.config.from_object(app_config[config_name])
     app.config.from_pyfile('config.py')
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
     db.init_app(app)
 
+    @app.route('/bucketlists/', methods=['POST', 'GET'])
+    def bucketlists():
+        if request.method == "POST":
+            name = str(request.data.get('name', ''))
+            if name:
+                bucketlist = Bucketlist(name=name)
+                bucketlist.save()
+                response = jsonify({
+                    'id': bucketlist.id,
+                    'name': bucketlist.name,
+                    'date_created': bucketlist.date_created,
+                    'date_modified': bucketlist.date_modified
+                })
+                response.status_code = 201
+                return response
+        else:
+            # GET
+            bucketlists = Bucketlist.get_all()
+            results = []
+
+            for bucketlist in bucketlists:
+                obj = {
+                    'id': bucketlist.id,
+                    'name': bucketlist.name,
+                    'date_created': bucketlist.date_created,
+                    'date_modified': bucketlist.date_modified
+                }
+                results.append(obj)
+            response = jsonify(results)
+            response.status_code = 200
+            return response
     return app
